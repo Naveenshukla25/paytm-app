c7a3e1291ebf15fe8fb2745d9a9420ee3b767daa
algoBhai <naveenshukla2501@gmail.com>
2025-11-04 00:17:21 +0530

    Fix :Render deployment

--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,14 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
+const PORT = process.env.PORT || 3000;
+
 app.use(cors());
 app.use(express.json());
 
 const mainRouter = require("./route/index");
 
 app.use("/api/v1", mainRouter);
-app.listen(3000, () => {
-  console.log("Server is running on port 3000");
+app.listen(PORT, () => {
+  console.log(`Server is running on port 3000 ${PORT}`);
 });
